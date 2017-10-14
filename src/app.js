b69f8d6bc8e6983817eb352d360a83a46c7bd1ab
Lucas Leandro <lucasleandro1204@gmail.com>
2017-10-14 00:36:29 -0300

    first

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import App from './components/App.vue';
+
+window.app = new Vue(App).$mount('#app');
---
+++ src/components/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <section id="component" :style="{ backgroundColor }">
+    <component :is="currentComponent"></component>
+  </section>
+</template>
+
+<script>
+  import Donate from './donate/Main.vue';
+
+  export default {
+    components: {
+      Donate,
+    },
+
+    data () {
+      return {
+        currentComponent: 'donate',
+        colors: {
+          donate: '#ECF0F1',
+        }
+      };
+    },
+
+    computed: {
+      backgroundColor () {
+        return this.colors[this.currentComponent];
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    box-sizing: border-box;
+
+    &::after,
+    &::before {
+      box-sizing: border-box;
+    }
+  }
+
+  #component {
+    display: flex;
+    min-height: 100vh;
+    padding: 50px 15px;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
