e272cdf0608fe015801a8a7d0c24054546b8a131
Lucas Leandro <lucasleandro1204@gmail.com>
2017-10-18 02:32:44 -0200

    simulate api request

--- src/components/donation/Main.vue
+++ src/components/donation/Main.vue
@@ -10,21 +10,34 @@
   import DonationBox from './Box.vue';
   import DonationList from './List.vue';
 
+  const donations = [
+    { date: Moment('20170703').format(), value: '25' },
+    { date: Moment('20150312').format(), value: '150' },
+    { date: Moment('20170613').format(), value: '400' },
+    { date: Moment('20170519').format(), value: '139' },
+    { date: Moment('20170225').format(), value: '230' },
+    { date: Moment('20141123').format(), value: '130' },
+  ];
+  
   export default {
     components: {
       DonationBox,
       DonationList,
     },
 
+    created () {
+      /*
+       * Simulate an API request.
+       */
+      setTimeout(() => {
+        this.donations = donations.sort((a, b) => Moment(b.date).diff(Moment(a.date))); 
+      }, 1500);
+    },
+
     data () {
       return {
-        deadLine: Moment().add(15, 'days').endOf('day').fromNow('dd'), // drop add latter
-        donations: [
-          { date: Date.now(), value: '1' },
-          { date: Date.now(), value: '12' },
-          { date: Date.now(), value: '30' },
-          { date: Date.now(), value: '150' },
-        ],
+        deadLine: Moment().add(13, 'days').endOf('day').fromNow('dd'), // drop add latter
+        donations: [],
       };
     },
   };
