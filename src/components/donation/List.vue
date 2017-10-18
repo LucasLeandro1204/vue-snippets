7e031236c46fa1b12c38c729157ecd25519dfabe
Lucas Leandro <lucasleandro1204@gmail.com>
2017-10-18 02:49:09 -0200

    working xd

--- src/components/donation/List.vue
+++ src/components/donation/List.vue
@@ -1,16 +1,39 @@
 <template>
   <article>
-    <transition-group tag="ul">
+    <transition-group name="list" tag="ul">
         <li :key="index" v-for="(donation, index) in donations">{{ donation.value }} <span></span></li>
     </transition-group>
   </article>
 </template>
 
 <script>
+  import Moment from 'moment';
   import Store from './store.js';
 
   export default {
     mixins: [Store],
+
+    computed: {
+      donationsChunkByYear () {
+        return this.donations.reduce((donations, donation) => {
+          const year = Moment(donation.date).format('Y');
+
+          if (! donations[year]) {
+            donations[year] = [];
+          }
+
+          donations[year].push(donation);
+
+          return donations;
+        }, {});
+      },
+
+      donationsAsArray () {
+        return Object.keys(this.donationsChunkByYear)
+          .sort((a, b) => b - a) // We have to order this shit again.
+          .map((date) => this.donationsChunkByYear[date]);
+      }
+    },
   }
 </script>
 
